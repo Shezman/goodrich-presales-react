91e4a0797abb3270d4e4745586966065a8803cc2
Sherman Tanuwidjaja <shermantanuwidjaja@gmail.com>
2019-09-29 01:30:34 +0800

    add build to git

---
+++ build/precache-manifest.ee511a42fac0a77507408b4cb325b886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31021f1c37498e9fc884d852f5a138b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "444551151941eecd8600",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "446df5a6c1c892fb33ef",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "444551151941eecd8600",
+    "url": "/static/js/2.c91ed7ee.chunk.js"
+  },
+  {
+    "revision": "446df5a6c1c892fb33ef",
+    "url": "/static/js/main.50c96b95.chunk.js"
+  },
+  {
+    "revision": "3a466e43b362ce512e5d",
+    "url": "/static/js/runtime~main.d12bc2e2.js"
+  }
+]);
